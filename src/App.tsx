4b21882af5d903896b8b3ea177ccc31d200f3933
Pedro Bigas <91469236+PedroBigas@users.noreply.github.com>
2022-09-13 00:09:14 -0300

    primeiros passos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import './input.css'
 
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
+import logoIMg from './assets/Logo.svg';
+import Game from './components/Games';
 
 function App() {
     return( 
-      <h1>Hello World NLW</h1>
+      <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+
+            <img src={logoIMg} alt="logo nlw eSports"/>
+
+            <h1 className='text-6xl text-white font-black mt-20' >
+              Seu <span className='text-transparent bg-nlw-gradient bg-clip-text '> duo </span> está aqui.
+              </h1>
+
+            <Game />
+
+
+          <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 '>
+            <div className='bg-[#2a2634] px-8 py-6 flex justify-between items-center'>
+              <div>
+                <strong className='text-2xl text-white font-bold block'>Não encontrou seu duo?</strong>
+                <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span> 
+              </div>  
+
+            <button className='py-3 px-4 bg-violet-500 hover:bg-violet-700 transition text-white rounded flex items-center gap-3'>
+              <MagnifyingGlassPlus size={24}/>
+              Publicar anúncio
+            </button>
+            </div>
+          </div>
+        </div>
       )
 }
 
